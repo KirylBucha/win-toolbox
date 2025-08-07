57738710037d8a7b1c3371ff2bd5d008e09fc1ef
kbucha <kbucha@datamola.com>
2025-08-07 19:55:03 +0100

    --remove programs from common

--- nixos/modules/programs/tui/kubernetes/default.nix
+++ nixos/modules/programs/tui/kubernetes/default.nix
@@ -5,36 +5,15 @@
   ...
 }: {
   # Install OpenShift CLI (OC), Kubectl, and Helm tools
-  environment.systemPackages = with pkgs; [
-    # Kubectl - available directly in nixpkgs
-    kubectl
-    
+  programs.kubectl = {
+    enable = true;
+  };
+  
+  home.packages = with pkgs; [
     # Helm - available directly in nixpkgs
     kubernetes-helm
     
-    # OpenShift CLI (OC) - custom download from specified URL
-#    (
-#      let
-#        version = "latest";
-#      in
-#      pkgs.runCommand "openshift-client-${version}" {
-#        nativeBuildInputs = [ pkgs.gnutar pkgs.curl ];
-#        # Use SSL certificates from the system
-#        SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-#      } ''
-#        # Create bin directory
-#        mkdir -p $out/bin
-#
-#        # Download the latest oc client
-#        curl -L https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz -o oc.tar.gz
-#
-#        # Extract and install
-#        tar -xzf oc.tar.gz -C $out/bin
-#        chmod +x $out/bin/oc
-#
-#        # Add version info
-#        $out/bin/oc version > $out/version.txt 2>&1 || true
-#      ''
-#    )
+    # OpenShift CLI (OC) - using nixpkgs if available
+#    openshift-client
   ];
 }
