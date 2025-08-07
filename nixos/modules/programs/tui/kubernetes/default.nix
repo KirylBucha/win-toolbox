59b9fe52c8804f8df8fbe859f2dd022868c48b02
kbucha <kbucha@datamola.com>
2025-08-07 19:56:03 +0100

    --remove programs from common

--- nixos/modules/programs/tui/kubernetes/default.nix
+++ nixos/modules/programs/tui/kubernetes/default.nix
@@ -5,15 +5,15 @@
   ...
 }: {
   # Install OpenShift CLI (OC), Kubectl, and Helm tools
-  programs.kubectl = {
-    enable = true;
-  };
-  
-  home.packages = with pkgs; [
-    # Helm - available directly in nixpkgs
-    kubernetes-helm
-    
-    # OpenShift CLI (OC) - using nixpkgs if available
+#  programs.kubectl = {
+#    enable = true;
+#  };
+#
+#  home.packages = with pkgs; [
+#    # Helm - available directly in nixpkgs
+#    kubernetes-helm
+#
+#    # OpenShift CLI (OC) - using nixpkgs if available
 #    openshift-client
-  ];
+#  ];
 }
