f52d1a507ae1d0deeffdd217cb772b7eae70dedf
kbucha <kbucha@datamola.com>
2025-08-10 00:10:58 +0100

    -- fix Terminal application

--- nixos/modules/nixos/home-manager.nix
+++ nixos/modules/nixos/home-manager.nix
@@ -45,13 +45,5 @@ in
     virt-manager.enable = false;
   };
 
-  # --------------------------------------------------------------------
-  # Configure Terminal.app Font
-  # --------------------------------------------------------------------
-  programs.terminal = {
-    enable = true; # This is required to manage Terminal.app settings
-    # --- FONT SETTINGS ---
-    fontSize = 14;
-  };
 
 }
