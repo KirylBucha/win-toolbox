0fa0fbd2bc3c11fefad49441077ac7a8d072bd3c
kbucha <kbucha@datamola.com>
2025-08-10 00:48:46 +0100

    -- add aliases -- add draft documentation

--- nixos/modules/programs/tui/zsh/default.nix
+++ nixos/modules/programs/tui/zsh/default.nix
@@ -91,10 +91,10 @@
       e = "exit";
       c = "clear";
       cs = "sudo nix-store --gc";
-      ll = "ls -l";
+      ll = "ls -la";
       cat = "bat";
       nxup = "cd ~/win-toolbox/nixos && git pull && sudo nixos-rebuild switch --flake .#foundation";
-      dwup = "cd ~/win-toolbox/nixos && git pull && sudo darwin-rebuild switch --flake .#darwin";
+      dwup = "cd ~/Work/win-toolbox/nixos && git pull && sudo darwin-rebuild switch --flake .#darwin";
       gitCommitUndo = "git reset --soft HEAD\\^";
       # nix garbage collect
       ncg = "nix-collect-garbage --delete-older-than 3d && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
