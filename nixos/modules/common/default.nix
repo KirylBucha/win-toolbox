a9da685398ef2b8bcde17bfeb346ee2252648936
kbucha <kbucha@datamola.com>
2025-08-09 20:02:21 +0100

    -- add Darwin Configuration to Start MacOS installation -- fix darwin.pc inputs

--- nixos/modules/common/default.nix
+++ nixos/modules/common/default.nix
@@ -1,125 +1,105 @@
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  vars,
-  ...
-} @ args:
-# Optional: give a name to the whole argument set
-{
-  imports = [
-    inputs.home-manager.nixosModules.home-manager
-  ];
-
-  system.autoUpgrade = {
-    enable = true;
-    allowReboot = true;
-  };
-
-  home-manager = {
-    extraSpecialArgs = {
-      inherit inputs outputs vars;
-    };
-    users.${vars.user.name} = import ../../modules/home;
-  };
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  nixpkgs = {
-    overlays = [
-      inputs.nix-vscode-extensions.overlays.default
+{ inputs, outputs, lib, config, pkgs, vars, ... } @ args:
+let
+  inherit (lib) mkIf mkMerge;
+in
+mkMerge [
+  {
+    imports = [
+      inputs.home-manager.nixosModules.home-manager
     ];
-    config = {
-      allowUnfree = true;
+
+    system.autoUpgrade = {
+      enable = true;
+      allowReboot = true;
     };
-  };
 
-  networking.networkmanager.enable = true;
-
-  time.timeZone = vars.system.timeZone;
-
-  i18n = {
-    defaultLocale = vars.system.locale;
-    extraLocaleSettings = {
-      LC_ADDRESS = vars.system.locale;
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
+    home-manager = {
+      extraSpecialArgs = {
+        inherit inputs outputs vars;
+      };
+      users.${vars.user.name} = import ../../modules/home;
     };
-  };
 
-  programs.nix-ld.enable = true;
+    nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  # Docker-Compose
-  virtualisation = {
-    libvirtd.enable = false;
-    docker.enable = true;
-    podman.enable = false;
-  };
+    nixpkgs = {
+      overlays = [
+        inputs.nix-vscode-extensions.overlays.default
+      ];
+      config = {
+        allowUnfree = true;
+      };
+    };
 
-  programs = {
-    virt-manager.enable = false;
-  };
+    # Cross-platform packages
+    environment = {
+      systemPackages = with pkgs; [
+        bat
+        zsh
+        tmux
+        fzf
+        file
+        git
+        btop
+        jq
+        neovim
+        vimPlugins.vim-plug
+        unzip
+        wget
+        curl
+        zip
+        tree
+        just
+        gcc
+        gnumake
+        kubectl
+      ];
+    };
+  }
 
-#  services = {
-#    xserver.enable = true;
-#    displayManager.sddm.enable = true;
-#    desktopManager.plasma6.enable = true;
-#
-#    xserver.xkb = {
-#      layout = "us";
-#      variant = "";
-#    };
-#
-#    pipewire = {
-#      enable = true;
-#      alsa.enable = true;
-#      alsa.support32Bit = true;
-#      pulse.enable = true;
-#    };
-#  };
+  # Linux-only settings (not available on Darwin)
+  (mkIf pkgs.stdenv.isLinux {
+    networking.networkmanager.enable = true;
 
-#  security.rtkit.enable = true;
+    time.timeZone = vars.system.timeZone;
 
-  users.users.${vars.user.name} = {
-#    initialPassword = "password";
-    isNormalUser = true;
-    description = "${vars.user.fullName}";
-    extraGroups = ["networkmanager" "wheel" "input" "docker"];
-    ignoreShellProgramCheck = true;
-    shell = pkgs.${vars.user.packages.shell};
-  };
+    i18n = {
+      defaultLocale = vars.system.locale;
+      extraLocaleSettings = {
+        LC_ADDRESS = vars.system.locale;
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+      };
+    };
 
-  environment = {
-    systemPackages = with pkgs; [
-      bat
-      zsh
-      tmux
-      fzf
-      file
-      git
-      btop
-      jq
-      neovim
-      vimPlugins.vim-plug
-      unzip
-      wget
-      curl
-      zip
-      tree
-      just
-      gcc
-      gnumake
-      kubectl
-    ];
-  };
+    programs.nix-ld.enable = true;
 
-  system.stateVersion = "24.05";
-}
+    # Docker-Compose and virtualization
+    virtualisation = {
+      libvirtd.enable = false;
+      docker.enable = true;
+      podman.enable = false;
+    };
+
+    programs = {
+      virt-manager.enable = false;
+    };
+
+    users.users.${vars.user.name} = {
+  #    initialPassword = "password";
+      isNormalUser = true;
+      description = "${vars.user.fullName}";
+      extraGroups = ["networkmanager" "wheel" "input" "docker"];
+      ignoreShellProgramCheck = true;
+      shell = pkgs.${vars.user.packages.shell};
+    };
+
+    system.stateVersion = "24.05";
+  })
+]
