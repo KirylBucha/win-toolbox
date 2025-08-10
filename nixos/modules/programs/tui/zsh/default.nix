6b3dad2ac7db275d1feca203f72c4aafe04ad246
bucha <kbucha@datamola.com>
2025-08-10 22:11:03 +0100

    -- trying to add fzf-tab to tmux

--- nixos/modules/programs/tui/zsh/default.nix
+++ nixos/modules/programs/tui/zsh/default.nix
@@ -44,8 +44,8 @@
           if [[ -n "$TMUX" ]]; then
             # Use fzf-tab's bundled helper to open a tmux popup for completion UI
             zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
-            # Some sensible default flags: reverse list, half-height preview with top border
-            zstyle ':fzf-tab:*' fzf-flags '--reverse --preview-window=down,50%,border-top'
+#            # Some sensible default flags: reverse list, half-height preview with top border
+#            zstyle ':fzf-tab:*' fzf-flags '--reverse --preview-window=down,50%,border-top'
           fi
         '';
 
