03715a8e0e72585766fbdf185ab8514f39b450bc
KBucha <KBucha@datamola.com>
2025-08-24 22:10:00 +0100

    -- fix apple terminal

--- nixos/modules/programs/tui/zsh/default.nix
+++ nixos/modules/programs/tui/zsh/default.nix
@@ -37,7 +37,10 @@
                 if [ -z "$TMUX" ]; then
                   if [[ "$OSTYPE" == darwin* ]]; then
                     case "''${TERM_PROGRAM:-}" in
-                      Alacritty|alacritty)
+                      Apple_Terminal)
+                        # Do nothing for Apple Terminal
+                      ;;
+                      *)
                         tmux attach -t default 2>/dev/null || tmux new -s default
                       ;;
                     esac
