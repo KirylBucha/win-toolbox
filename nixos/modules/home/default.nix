6b73d3bbc3c06c482e89919e7a09afc474193357
KBucha <KBucha@datamola.com>
2025-11-22 19:28:16 +0000

    fix alacritty

--- nixos/modules/home/default.nix
+++ nixos/modules/home/default.nix
@@ -46,6 +46,10 @@
     stateVersion = "25.05"; # home.stateVersion
   };
 
+  home.sessionPath = [
+    "$HOME/.cargo/bin/"
+  ];
+
   # Marked broken Oct 20, 2022 check later to remove this
   # https://github.com/nix-community/home-manager/issues/3344
   manual.manpages.enable = false;
